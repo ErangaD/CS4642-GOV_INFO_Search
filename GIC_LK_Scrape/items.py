30e6d1c93e93984bdcc7d9482e5fdd4d9c492e9f
ErangaD <erangadulshan.14@cse.mrt.ac.lk>
2018-07-09 17:34:34 +0530

    sinhala scraped data

--- GIC_LK_Scrape/items.py
+++ GIC_LK_Scrape/items.py
@@ -7,8 +7,7 @@
 
 import scrapy
 
-
-class GicLkScrapeItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+class OrgItem(scrapy.Item):
+    text = scrapy.Field()
+    link = scrapy.Field()
     pass
--- GIC_LK_Scrape/spiders/SinhalSpider.py
+++ GIC_LK_Scrape/spiders/SinhalSpider.py
@@ -3,37 +3,49 @@ from scrapy.spiders import Rule
 from scrapy.linkextractors import LinkExtractor
 from w3lib.html import remove_tags, remove_tags_with_content
 from scrapy.selector import Selector
+from GIC_LK_Scrape.items import OrgItem
+import csv
 import re
 
-class SinhalaSpider(CrawlSpider):
+class EnglishSpider(CrawlSpider):
     name = 'sinhalaCrawler'
     allowed_domains = ["gic.gov.lk"]
-    start_urls = ['http://www.gic.gov.lk/gic/index.php/en/component/org/']
+    start_urls = ['http://www.gic.gov.lk/gic/index.php/si/component/org/']
 
     rules = (
         Rule(LinkExtractor(), callback = 'parse_item', follow=True, process_request='process_request'),
     )
 
     count = 1
+    file_name = open('Output_file_sinhala.csv', 'w')  # Output_file.csv is name of output file
+
+    fieldnames = ['text', 'link']  # adding header to file
+    writer = csv.DictWriter(file_name, fieldnames=fieldnames)
+    writer.writeheader()
     def parse_item(self, response):
-        if ("si" in response.request.url):
-            return
         sel = Selector(response)
-        filename = "test/sinhala/T" + str(self.count) + ".txt"
-        tags_removed_text = remove_tags(remove_tags_with_content(sel.xpath('//body').extract()[0],which_ones=('script',)))
+        filename = "test/english/T" + str(self.count) + ".txt"
+        tags_removed_text = remove_tags(remove_tags_with_content(sel.xpath('//*[@id="ja-content"]').extract()[0],which_ones=('script',)))
         tabs_removed_text = tags_removed_text.replace("\t", '').replace('\r', '')
         newLineRemovedText = re.sub(r'(\s*\n\s*)\1*', '\n', tabs_removed_text)
-        with open(filename, "w") as out_file:
-            for item in newLineRemovedText.split('\n'):
-                out_file.write("%s\n" % item.strip())
+        # with open(filename, "w") as out_file:
+        #     for item in newLineRemovedText.split('\n'):
+        #         out_file.write("%s\n" % item.strip())
+        #
+        # followed_urls = "followedTestURLs/englishUrls.txt"
+        # with open(followed_urls, "a") as out_file:
+        #     out_file.write("%s\n" % response.request.url)
+        # docum = OrgItem()
+        # docum['text'] = newLineRemovedText
+        # docum['link'] = response.request.url
 
-        followed_urls = "followedTestURLs/sinhalaUrls.txt"
-        with open(followed_urls, "a") as out_file:
-            out_file.write("%s\n" % response.request.url)
+        self.writer.writerow(
+                {'text': newLineRemovedText, 'link': response.request.url})  # writing data into file.
         self.count += 1
         print(self.count)
 
     def process_request(self, request):
         url = request.url
-        if("component/org" in url) :
-            return request
+        if("component/org" in url):
+            if ("/si/" in url):
+                return request
